7ef0e092f56774c4368bf71fd9f75084be0d5d51
likuku-new-macbook <likuku.public@gmail.com>
2017-12-18 02:20:05 +0800

    test_resize_images_for_blog.py fixed test_make_str_list_cmd_resize_images()

--- test_resize_images_for_blog.py
+++ test_resize_images_for_blog.py
@@ -9,7 +9,7 @@ def create_test_env():
     except Exception as e:
         pass
     try:
-        for _file in ['1.jpg','2.jpg','3.jpg']:
+        for _file in ['.DS_Store.jpg','1.jpg','2.jpg','3.jpg']:
             _path = os.path.join(_dir,_file)
             _f = os.open(_path,os.O_CREAT)
             os.close(_f)
@@ -19,7 +19,7 @@ def create_test_env():
 def clean_test_env():
     _dir = 'tmp'
     try:
-        for _file in ['1.jpg','2.jpg','3.jpg']:
+        for _file in ['.DS_Store.jpg','1.jpg','2.jpg','3.jpg']:
             _path = os.path.join(_dir,_file)
             os.remove(_path)
     except Exception as e:
@@ -42,7 +42,7 @@ class Test_resize_images_for_blog(unittest.TestCase):
         self.assertEqual(False,
                          check_str_raw_src_media_path('.13123241'))
         self.assertEqual(True,
-                         check_str_raw_src_media_path('.'))
+                         check_str_raw_src_media_path('tmp'))
 
     def test_rebuild_str_src_media_path(self):
         self.assertEqual('/path/subPath',
@@ -58,15 +58,20 @@ class Test_resize_images_for_blog(unittest.TestCase):
         self.assertEqual(['1.jpg','2.jpg','3.jpg'],
              get_str_list_src_images('tmp'))
 
-    def make_str_list_cmd_resize_images(self):
+    def test_make_str_list_cmd_resize_images(self):
         # make_str_list_cmd_resize_images(_path,_dir,_src_image,_out_w,_out_h)
         _path = '/path/subPath'
         _dir = 'full'
         _src_image = '1.jpg'
         _out_w = '1920'
         _out_h = '1080'
+        _str_src_path = os.path.join(_path,_src_image)
         _str_output_path = os.path.join(_path,_dir,_src_image)
+        _str_vf = ('scale=w={_out_w}:h={_out_h}:force_original_aspect_ratio=decrease,'
+                   'pad=x=(ow-iw)/2:y=(oh-ih)/2:w={_out_w}:h={_out_h}')
+        _str_vf= _str_vf.format_map(vars())
         self.assertEqual(['ffmpeg',
+                          '-y',
                           '-i',_str_src_path,
                           '-pix_fmt','yuvj420p',
                           '-vf',_str_vf,
