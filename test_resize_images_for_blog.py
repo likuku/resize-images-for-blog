c912c63d948c88a1d971f166a426fa3aa28cde2c
likuku-macbook-pro <likuku.public@gmail.com>
2017-12-21 21:13:59 +0800

    test_resize_images_for_blog.py add some test_rules empty

--- test_resize_images_for_blog.py
+++ test_resize_images_for_blog.py
@@ -64,25 +64,38 @@ class Test_resize_images_for_blog(unittest.TestCase):
         self.assertEqual(['1.jpg','2.jpg','3.jpg'],
              get_str_list_src_images('tmp'))
 
-    def test_make_str_list_cmd_resize_images(self):
+    def test_make_str_list_cmd_resize_images_fulls(self):
         # make_str_list_cmd_resize_images(_path,_dir,_src_image,_out_w,_out_h)
         _path = '/path/subPath'
         _dir = 'fulls'
         _src_image = '1.jpg'
-        _out_w = '1920'
-        _out_h = '1080'
+        _out_w = '1200'
+        _out_h = '750'
         _str_src_path = os.path.join(_path,_src_image)
         _str_output_path = os.path.join(_path,_dir,_src_image)
+        '''
         _str_vf = ('scale=w={_out_w}:h={_out_h}:force_original_aspect_ratio=decrease,'
                    'pad=x=(ow-iw)/2:y=(oh-ih)/2:w={_out_w}:h={_out_h}')
         _str_vf= _str_vf.format_map(vars())
-        self.assertEqual(['ffmpeg',
-                          '-i',_str_src_path,
-                          '-pix_fmt','yuvj420p',
-                          '-vf',_str_vf,
-                          '-q:v','2',
-                          _str_output_path],
-             make_str_list_cmd_resize_images(_path,_dir,_src_image,_out_w,_out_h))
+        '''
+        self.assertEqual(['sips',
+                          _str_src_path,
+                          '-s','format jpeg',
+                          '--resampleHeight','750',
+                          '--padToHeightWidth','750 1200',
+                          '--padColor','333333',
+                          '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
+                          '--out',_str_output_path],
+             make_str_list_cmd_resize_images_fulls(_path,_dir,_src_image,_out_w,_out_h))
+
+    def test_make_str_list_cmd_resize_images_thumbs(self):
+        pass
+
+    def test_make_str_list_cmd_resize_images_thumbs_portrait(self):
+        pass
+
+    def test_check_bool_image_is_portrait(self):
+        pass
 
 
 if __name__ == '__main__':
