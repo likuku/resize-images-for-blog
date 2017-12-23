8b6adb2e27d912d4f8fb948fe00790a574726fde
likuku-new-macbook <likuku.public@gmail.com>
2017-12-23 16:19:42 +0800

    update _str_src_path,_str_output_path for Special fileNames

--- test_resize_images_for_blog.py
+++ test_resize_images_for_blog.py
@@ -37,7 +37,8 @@ class Test_resize_images_for_blog(unittest.TestCase):
                      'file name.jpg',
                      'file-name.jpg',
                      'file,name.jpg',
-                     'file(name).jpg']
+                     'file(name).jpg',
+                     '文件,-( ).名..jpg']
     def setUp(self):
         create_test_env(self.tmp_file_list)
 
@@ -76,62 +77,69 @@ class Test_resize_images_for_blog(unittest.TestCase):
         # make_str_list_cmd_resize_images(_path,_dir,_src_image,_out_w,_out_h)
         _path = '/path/subPath'
         _dir = 'fulls'
-        _src_image = '1.jpg'
         _out_w = '1200'
         _out_h = '750'
-        _str_src_path = os.path.join(_path,_src_image)
-        _str_output_path = os.path.join(_path,_dir,_src_image)
-        '''
-        _str_vf = ('scale=w={_out_w}:h={_out_h}:force_original_aspect_ratio=decrease,'
-                   'pad=x=(ow-iw)/2:y=(oh-ih)/2:w={_out_w}:h={_out_h}')
-        _str_vf= _str_vf.format_map(vars())
-        # maybe use build str with keyWord,then split with keyWord to make list
-        '''
-        self.assertEqual(['sips',
-                          _str_src_path,
-                          '-s','format','jpeg',
-                          '--resampleHeight','750',
-                          '--padToHeightWidth','750','1200',
-                          '--padColor','0D0D0D',
-                          '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
-                          '--out',_str_output_path],
-             make_str_list_cmd_resize_images_fulls(_path,_dir,_src_image,_out_w,_out_h))
+        for _src_image in self.tmp_file_list[1:]:
+            _str_src_path = os.path.join(_path,_src_image)
+            _str_output_path = os.path.join(_path,_dir,_src_image)
+            _str_src_path = '"' + os.path.join(_path,_src_image) + '"'
+            _str_output_path = '"' + os.path.join(_path,_dir,_src_image) + '"'
+            print(_str_src_path,_str_src_path)
+            '''
+            _str_vf = ('scale=w={_out_w}:h={_out_h}:force_original_aspect_ratio=decrease,'
+                       'pad=x=(ow-iw)/2:y=(oh-ih)/2:w={_out_w}:h={_out_h}')
+            _str_vf= _str_vf.format_map(vars())
+            # maybe use build str with keyWord,then split with keyWord to make list
+            '''
+            self.assertEqual(['sips',
+                              _str_src_path,
+                              '-s','format','jpeg',
+                              '--resampleHeight','750',
+                              '--padToHeightWidth','750','1200',
+                              '--padColor','0D0D0D',
+                              '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
+                              '--out',_str_output_path],
+                 make_str_list_cmd_resize_images_fulls(_path,_dir,_src_image,_out_w,_out_h))
 
     def test_make_str_list_cmd_resize_images_thumbs(self):
         # make_str_list_cmd_resize_images_thumbs(_path,_dir,_src_image,_out_w,_out_h)
         _path = '/path/subPath'
         _dir = 'thumbs'
-        _src_image = '1.jpg'
         _out_w = '300'
         _out_h = '300'
-        _str_src_path = os.path.join(_path,_src_image)
-        _str_output_path = os.path.join(_path,_dir,_src_image)
-        self.assertEqual(['sips',
-                          _str_src_path,
-                          '-s','format','jpeg',
-                          '--resampleHeight','300',
-                          '--cropToHeightWidth','300','300',
-                          '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
-                          '--out',_str_output_path],
-             make_str_list_cmd_resize_images_thumbs(_path,_dir,_src_image,_out_w,_out_h))
+        for _src_image in self.tmp_file_list[1:]:
+            _str_src_path = '"' + os.path.join(_path,_src_image) + '"'
+            _str_output_path = '"' + os.path.join(_path,_dir,_src_image) + '"'
+            print(_str_src_path,_str_src_path)
+            self.assertEqual(['sips',
+                              _str_src_path,
+                              '-s','format','jpeg',
+                              '--resampleHeight','300',
+                              '--cropToHeightWidth','300','300',
+                              '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
+                              '--out',_str_output_path],
+                 make_str_list_cmd_resize_images_thumbs(_path,_dir,_src_image,_out_w,_out_h))
 
     def test_make_str_list_cmd_resize_images_thumbs_portrait(self):
         # make_str_list_cmd_resize_images_thumbs_portrait(_path,_dir,_src_image,_out_w,_out_h)
         _path = '/path/subPath'
         _dir = 'thumbs'
-        _src_image = '1.jpg'
         _out_w = '300'
         _out_h = '300'
-        _str_src_path = os.path.join(_path,_src_image)
-        _str_output_path = os.path.join(_path,_dir,_src_image)
-        self.assertEqual(['sips',
-                          _str_src_path,
-                          '-s','format','jpeg',
-                          '--resampleWidth','300',
-                          '--cropToHeightWidth','300','300',
-                          '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
-                          '--out',_str_output_path],
-             make_str_list_cmd_resize_images_thumbs_portrait(_path,_dir,_src_image,_out_w,_out_h))
+        for _src_image in self.tmp_file_list[1:]:
+            _str_src_path = os.path.join(_path,_src_image)
+            _str_output_path = os.path.join(_path,_dir,_src_image)
+            _str_src_path = '"' + os.path.join(_path,_src_image) + '"'
+            _str_output_path = '"' + os.path.join(_path,_dir,_src_image) + '"'
+            print(_str_src_path,_str_src_path)
+            self.assertEqual(['sips',
+                              _str_src_path,
+                              '-s','format','jpeg',
+                              '--resampleWidth','300',
+                              '--cropToHeightWidth','300','300',
+                              '-m','/System/Library/Colorsync/Profiles/sRGB Profile.icc',
+                              '--out',_str_output_path],
+                 make_str_list_cmd_resize_images_thumbs_portrait(_path,_dir,_src_image,_out_w,_out_h))
 
     def test_check_bool_image_is_portrait(self):
         # check_bool_image_is_portrait(_path,_src_image)
@@ -141,7 +149,6 @@ class Test_resize_images_for_blog(unittest.TestCase):
             check_bool_image_is_portrait('.','300x200.jpg'))
         self.assertEqual(False,
             check_bool_image_is_portrait('.','300x300.jpg'))
-
 
 
 if __name__ == '__main__':
